Correct normalization

build_bicubic.py
@@ -2,26 +2,72 @@ import numpy as np
 import glob
 import scipy.ndimage
 
+OPERATION = "downscale"
+
 
 # Define bicubic function
 def bicubic_upscale(input_file, output_file, scale_factor):
     # Load the hyperspectral image from the .npy file
     snapshot_image = np.load(input_file)
     
+    # Perform bicubic upscaling
     upscaled_image = scipy.ndimage.zoom(snapshot_image, (scale_factor, scale_factor, 1), order=3)
+    
+    # Normalize the upscaled image to the range [0.0, 1.0]
+    upscaled_image = (upscaled_image - np.min(upscaled_image)) / (np.max(upscaled_image) - np.min(upscaled_image))
     
     # Save the upscaled image to a new .npy file
     np.save(output_file, upscaled_image)
-    print(f"Upscaled image saved to {output_file}")
+    print(f"Upscaled image saved to {output_file} with min: {upscaled_image.min()} and max: {upscaled_image.max()}")
+    print(" ")
 
 
 
-# Specify paths
-ss_files = glob.glob("data/Processed/train/**")
-scale_factor = 480/210
-# scale_factor = 240/210 
+# Define bicubuc downscale function
+def bicubic_downscale(input_file, output_file, scale_factor):
+    # Load the hyperspectral image from the .npy file
+    hr_image = np.load(input_file)
+    
+    # Perform bicubic downscaling
+    downscaled_image = scipy.ndimage.zoom(hr_image, (1/scale_factor, 1/scale_factor, 1), order=3)
+    
+    # Normalize the downscaled image to the range [0.0, 1.0]
+    downscaled_image = (downscaled_image - np.min(downscaled_image)) / (np.max(downscaled_image) - np.min(downscaled_image))
+    
+    # Save the downscaled image to a new .npy file
+    np.save(output_file, downscaled_image)
+    print(f"Downscaled image saved to {output_file} with min: {downscaled_image.min()} and max: {downscaled_image.max()}")
+    print(" ")
 
 
-for f in ss_files:
-    print("Processing file: " + f + "... (" + str(ss_files.index(f)+1) + "/" + str(len(ss_files)) + ")")
-    bicubic_upscale(f, "data/Processed/bicubic_480/" + f[21:], scale_factor=scale_factor)
+
+
+if OPERATION == "upscale":
+
+    # Specify paths
+    ss_files = glob.glob("data/processed/full_hsi/test/lr/**")
+
+    # Set goal size
+    # goal_size = 240
+    goal_size = 480
+    scale_factor = goal_size / 210 
+
+
+    for f in ss_files:
+        print("Processing file: " + f + "... (" + str(ss_files.index(f)+1) + "/" + str(len(ss_files)) + ")")
+        bicubic_upscale(f, "data/processed/full_hsi/test/bi_" + str(goal_size) + f[31:], scale_factor=scale_factor)
+
+
+elif OPERATION == "downscale":
+
+    # Specify paths
+    hr_files = glob.glob("data/processed/full_hsi/hr/**")
+
+    # Set goal size
+    goal_size = 240
+    scale_factor = 480 / goal_size
+
+
+    for f in hr_files:
+        print("Processing file: " + f + "... (" + str(hr_files.index(f)+1) + "/" + str(len(hr_files)) + ")")
+        bicubic_downscale(f, "data/processed/full_hsi/synth_" + str(goal_size) + f[26:], scale_factor=scale_factor)

build_train.py
@@ -6,11 +6,11 @@ import numpy as np
 SQUARE = True
 
 # Specify paths
-ss_files = glob.glob("data/Snapshot/processed/train/**/")
+ss_files = glob.glob("data/raw/Snapshot/processed/train/**/")
 
 # Specify camera calibration files
-mtx_path = 'calibration/snapshot_matrix.npy'
-dist_path = 'calibration/snapshot_dist.npy'
+mtx_path = 'data/raw/calibration/snapshot_matrix.npy'
+dist_path = 'data/raw/calibration/snapshot_dist.npy'
 
 
 if SQUARE == True:
@@ -31,4 +31,4 @@ for f in ss_files:
                                                     ss_y_off=ss_y_off)
 
     # Save as array
-    np.save("data/Processed/train/" + f[30:-1] + ".npy", processed_img)
+    np.save("data/processed/full_hsi/lr/" + f[34:-1] + ".npy", processed_img)

build_validation.py
@@ -6,14 +6,14 @@ import numpy as np
 SQUARE = True
 
 # Load all hdf5 files in a directory
-hdf5_files = glob.glob('data/FX10/*.hdf5')
+hdf5_files = glob.glob('data/raw/FX10/*.hdf5')
 
 # Load 1 hdf5 file as test
 # hdf5_files = ['data/FX10/spelt1_l.hdf5']
 
 # Specify camera calibration files
-mtx_path = 'calibration/hrHSI_matrix.npy'
-dist_path = 'calibration/hrHSI_dist.npy'
+mtx_path = 'data/raw/calibration/hrHSI_matrix.npy'
+dist_path = 'data/raw/calibration/hrHSI_dist.npy'
 
 # Snapshot wavelengths
 ss_wavelengths = [667, 679, 691, 703, 715, 
@@ -56,7 +56,7 @@ for f in hdf5_files:
                                                    ss_wavelengths=ss_wavelengths)
 
     # Save as array
-    np.save("data/Processed/val/" + f[10:-5] + ".npy", processed_img)
+    np.save("data/processed/full_hsi/hr/" + f[14:-5] + ".npy", processed_img)
 
 
 hr_wavelenghts = [666, 679, 690, 704, 715, 

preprocessing.py
@@ -130,11 +130,11 @@ def preprocessSnapshot(ss_path, mtx_path, dist_path, ss_x_off, ss_y_off):
     x, y, w, h = roi
     dst = dst[y:y+h, x:x+w]
 
-    # Normalize hypercube
-    dst = dst / np.max(dst)
-
     # Crop the image for 1.9 aspect ratio
     dst = dst[ss_y_off[0]:ss_y_off[1], ss_x_off[0]:ss_x_off[1], :]
+
+    # Normalize hypercube to [0, 1]
+    dst = (dst - np.min(dst)) / (np.max(dst) - np.min(dst))
 
     return dst, wavelengths
 
@@ -230,7 +230,7 @@ def preprocessFullHSI(path_to_hdf5, mtx_path, dist_path, hr_x_off, hr_y_off, rot
         new_y = ((ss_shape[0] / ss_shape[1]) * img.shape[1])
         img = transform.resize(img, (new_y, img.shape[1], img.shape[2]), order=1)
 
-        # Normalize hypercube
-        img = img / np.max(img)
+        # Normalize hypercube to [0, 1]
+        img = (img - np.min(img)) / (np.max(img) - np.min(img))
 
         return img, hwavelengths_filtered