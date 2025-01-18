8bbb8a9978bd8490e9ac667f41874464520d3f6b
Amar Music <amar.music@live.nl>
2025-01-18 23:43:07 +0100

    Test new models

--- Real-ESRGAN/inference_realesrgan.py
+++ Real-ESRGAN/inference_realesrgan.py
@@ -61,81 +61,11 @@ def main():
 
     # determine models according to model names
     args.model_name = args.model_name.split('.')[0]
-    # if args.model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
-    #     model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-    #     netscale = 4
-    #     file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
-    # elif args.model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
-    #     model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-    #     netscale = 4
-    #     file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth']
-    # elif args.model_name == 'RealESRGAN_x4plus_anime_6B':  # x4 RRDBNet model with 6 blocks
-    #     model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
-    #     netscale = 4
-    #     file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth']
-    # elif args.model_name == 'RealESRGAN_x2plus':  # x2 RRDBNet model
-    #     model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-    #     netscale = 2
-    #     file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth']
-    # elif args.model_name == 'realesr-animevideov3':  # x4 VGG-style model (XS size)
-    #     model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
-    #     netscale = 4
-    #     file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-animevideov3.pth']
-    # elif args.model_name == 'realesr-general-x4v3':  # x4 VGG-style model (S size)
-    #     model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
-    #     netscale = 4
-    #     file_url = [
-    #         'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-wdn-x4v3.pth',
-    #         'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth']
-    # elif args.model_name == 'RealESRGAN_2xHSI':  # x2 HSI
-    #     model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-    #     netscale = 2
-    #     file_url = ['none']
-    if args.model_name == 'RealESRGAN_x2plus':  # x2 RRDBNet model
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_grayimages_x2_5000':  # x2 HSI
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_rgbarray_x2_5000':  # x2 HSI
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_x2_v3_50000':  # x2 HSI
+    if args.model_name == 'HSI_x2_real':  # x2 HSI
         model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
         netscale = 2
         file_url = ['none']
-    elif args.model_name == 'HSI_x2_synth_50000':  # x2 HSI
-        model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_x2_10000':  # x2 HSI
-        model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_x2_v4_10000':  # x2 HSI
-        model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_x2_ft2_40000':  # x2 HSI
-        model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=96, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_x2_val3_150000':  # x2 HSI
-        model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_x2_val4_150000':  # x2 HSI
-        model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_x2_val3_synth_50000':  # x2 HSI
-        model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['none']
-    elif args.model_name == 'HSI_x2_val6_150000':  # x2 HSI
+    elif args.model_name == 'HSI_x2_synth':  # x2 HSI
         model = RRDBNet(num_in_ch=24, num_out_ch=24, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
         netscale = 2
         file_url = ['none']
